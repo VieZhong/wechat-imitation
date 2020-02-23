5c5639f00e3fe65ce49623e4d1deb9f8d1f99c8c
VieZhong <zyw13579@qq.com>
2020-02-23 21:20:47 +0800

    fix: id为空的bug

--- src/unit/constant/index.js
+++ src/unit/constant/index.js
@@ -5,6 +5,10 @@ export const prototal = process.env.NODE_ENV == 'production' ? 'wss' : 'ws';
 export const ws = new WebSocket(`${prototal}://${hostname}/api/communication/chat`);
 
 ws.onopen = function () {
+    const user = JSON.parse(window.sessionStorage.getItem('userInfo'));
+    if(!user || !user.id) {
+        return;
+    }
     ws.send(JSON.stringify({
         type: 'connection',
         by: JSON.parse(window.sessionStorage.getItem('userInfo')).id
